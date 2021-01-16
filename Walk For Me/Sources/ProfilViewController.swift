6f139bb8fed5c8b7bbedadea29ec8a6eb477be98
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-16 16:09:55 +0800

    set method to change appearance

--- Walk For Me/Sources/ProfilViewController.swift
+++ Walk For Me/Sources/ProfilViewController.swift
@@ -16,8 +16,23 @@ class ProfilViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.title = "Accueil"
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
+         
     }
 
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return profilArguments.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return ""
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
 }
 
