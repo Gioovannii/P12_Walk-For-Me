ff695660a601d05faae7742704b9f881ca1b82c2
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-23 16:10:52 +0800

    set center and a instance of location manager

--- Walk For Me/AppDelegate.swift
+++ Walk For Me/AppDelegate.swift
@@ -12,6 +12,8 @@ import UserNotifications
 @main
 @available(iOS 13.0, *)
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    let center = UNUserNotificationCenter.current()
+    let locationManager = CLLocationManager()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
