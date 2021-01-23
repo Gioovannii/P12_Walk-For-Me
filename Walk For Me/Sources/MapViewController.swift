edc48bbd742507e00122c14e928fc751c4ad1562
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-23 16:19:06 +0800

    set outlet an action for map and add location

---
+++ Walk For Me/Sources/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  Walk For Me
+//
+//  Created by Giovanni Gaffé on 2021/1/23.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var addPlacesButton: UIBarButtonItem!
+}
