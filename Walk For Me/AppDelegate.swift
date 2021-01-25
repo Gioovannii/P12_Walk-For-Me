08dfed1e465f946120f193c9776d3fb872fb33dd
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-25 16:52:17 +0800

    Remove unused code

--- Walk For Me/AppDelegate.swift
+++ Walk For Me/AppDelegate.swift
@@ -12,93 +12,25 @@ import UserNotifications
 @main
 @available(iOS 13.0, *)
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    let center = UNUserNotificationCenter.current()
-    let locationManager = CLLocationManager()
-    static let geoCoder = CLGeocoder()
-    
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
-        }
-        locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
-        locationManager.delegate = self
-        
-        locationManager.distanceFilter = 35
-        //locationManager.allowsBackgroundLocationUpdates = true
-        locationManager.startUpdatingLocation()
-        
+        // Override point for customization after application launch.
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
 
-@available(iOS 13.0, *)
-extension AppDelegate: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
-        // Create CLLocation from coordinate of CLLVisit
-        let clLocation = CLLocation(latitude: visit.coordinate.latitude, longitude: visit.coordinate.longitude)
-        AppDelegate.geoCoder.reverseGeocodeLocation(clLocation) { placemarks, _ in
-            if let place = placemarks?.first {
-                let description = "\(place)"
-                self.newVisitReceive(visit, description: description)
-            }
-        }
-        // Get location description
-    }
-    
-    func newVisitReceive(_ visit: CLVisit, description: String) {
-        let location = Location(visit: visit, descriptionString: description)
-        let content  = UNMutableNotificationContent()
-        content.title = "New journal entry 📌"
-        content.body = location.description
-        content.sound = .default
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-        let request = UNNotificationRequest(identifier: location.dateString, content: content, trigger: trigger)
-        
-        center.add(request, withCompletionHandler: nil)
-        
-        // Save visit on disk
-    }
-}
 
-final class FakeVisit: CLVisit {
-    private let myCoordinates: CLLocationCoordinate2D
-    private let myArrivalDate: Date
-    private let myDepartureDate: Date
-    
-    override var coordinate: CLLocationCoordinate2D {
-        return myCoordinates
-    }
-    
-    override var arrivalDate: Date {
-        return myArrivalDate
-    }
-    
-    
-    override var departureDate: Date {
-        return myDepartureDate
-    }
-    
-    init(coordinates: CLLocationCoordinate2D, arrivalDate: Date, departureDate: Date) {
-        myCoordinates = coordinates
-        myArrivalDate = arrivalDate
-        myDepartureDate = departureDate
-        super.init()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
+
