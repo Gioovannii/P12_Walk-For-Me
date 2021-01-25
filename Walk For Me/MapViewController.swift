95611610eeb7abf5ba529ab8c234681c94bd2ab8
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-25 16:53:37 +0800

    Remove outlet mapView make app crash

--- Walk For Me/MapViewController.swift
+++ Walk For Me/MapViewController.swift
@@ -10,13 +10,12 @@ import MapKit
 
 class MapViewController: UIViewController {
 
-    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.userTrackingMode = .follow
     }
     
-    @IBAction func addItemPRessed(_ sender: Any) {
+    @IBAction func addItemPressed(_ sender: Any) {
         
     }
 }
