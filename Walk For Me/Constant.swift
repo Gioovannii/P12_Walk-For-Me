8c852ff0998f6042b9da433f87baf428bd49225a
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-26 20:38:41 +0800

    et some constant to avoid miss typing

---
+++ Walk For Me/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  Walk For Me
+//
+//  Created by Giovanni Gaffé on 2021/1/26.
+//
+
+import Foundation
+
+struct Constant {
+    static let cellIdentifier = "Walk Cell"
+    static let performToMap = "CellToMap"
+    
+    struct Config {
+       
+    }
+}
