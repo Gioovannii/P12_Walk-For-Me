7b31c699e4082c0ae2c6884a63363c3c70d81267
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-27 16:19:02 +0800

    set tag with parameter of the method

--- Walk For Me/Sources/UpdateProfilController.swift
+++ Walk For Me/Sources/UpdateProfilController.swift
@@ -48,8 +48,8 @@ class UpdateProfilController: UITableViewController {
         let alertController = UIAlertController(title: "Choisissez votre sexe" , message: "\n\n\n\n\n\n\n", preferredStyle: .alert)
         
         let pickerFrame = UIPickerView(frame: CGRect(x: 5, y: 20, width: 250, height: 140))
-        pickerFrame.tag = 2
-        alert.view.addSubview(pickerFrame)
+        pickerFrame.tag = tag
+        alertController.view.addSubview(pickerFrame)
         pickerFrame.dataSource = self
         pickerFrame.delegate = self
         
