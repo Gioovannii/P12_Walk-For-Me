2c8bbee4c94c4bcce220f8a904a47c7f02f89be9
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-02-01 09:14:19 +0800

    create a Position of the user

---
+++ Walk For Me/Model/PositionUser.swift
@@ -0,0 +1,16 @@
+//
+//  PositionUser.swift
+//  Walk For Me
+//
+//  Created by Giovanni Gaffé on 2021/1/30.
+//
+
+import Foundation
+import CoreLocation
+
+class PositionUser {
+    
+    var location = CLLocation()
+    var locations = [CLLocation]()
+
+}
