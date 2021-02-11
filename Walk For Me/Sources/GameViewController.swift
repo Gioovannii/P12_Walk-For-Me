9efc1638a22d285fa3da99b7133c03d5f22250f8
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-02-11 13:41:12 +0800

    connect outlets for slot available and create file

---
+++ Walk For Me/Sources/GameViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GameViewController.swift
+//  Walk For Me
+//
+//  Created by Giovanni Gaffé on 2021/2/11.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var slotButton1: UIButton!
+    @IBOutlet weak var slotButton2: UIButton!
+    @IBOutlet weak var slotButton3: UIButton!
+    @IBOutlet weak var slotButton4: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
