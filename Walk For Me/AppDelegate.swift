4cd81e9dfb07c7fac72a7284957dce7a2bb8bc34
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-02-13 21:03:16 +0800

    indent

--- Walk For Me/AppDelegate.swift
+++ Walk For Me/AppDelegate.swift
@@ -11,7 +11,6 @@ import UIKit
 @available(iOS 13.0, *)
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
