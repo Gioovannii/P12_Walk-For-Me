c688edc58aaca6d3411cc414323fc9639210caa7
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-02-22 11:30:15 +0800

    Set correct color

--- Walk For Me/Helpers/Extensions/UIViewController + Alert.swift
+++ Walk For Me/Helpers/Extensions/UIViewController + Alert.swift
@@ -11,10 +11,10 @@ extension UIViewController {
     func presentAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Annuler", style: .cancel))
-        
-        alert.view.tintColor = UIColor.purple
-        alert.view.backgroundColor = UIColor.cyan
+
         alert.view.layer.cornerRadius = 70
+        alert.view.tintColor = UIColor(red: 0.690, green: 0.469, blue: 0.777, alpha: 1.0)
+        alert.view.backgroundColor = UIColor(red: 0.290, green: 0.412, blue: 0.714, alpha: 1.0)
         
         present(alert, animated: true)
     }}
