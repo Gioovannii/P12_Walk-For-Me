77951163fa92075d3172d5bba2433274cf899e54
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-03-12 15:15:33 +0800

    use force unwrapped for now

--- Walk For Me/Sources/Pace + Map/TrackHistoryTableViewController.swift
+++ Walk For Me/Sources/Pace + Map/TrackHistoryTableViewController.swift
@@ -54,8 +54,8 @@ final class TrackHistoryTableViewController: UITableViewController {
             print("Looping sessions \(track.locations as Any)")
             count.append("\(countTrack + 1)")
             paceNumber.append("0")
-            guard let last = Int(count.last ?? "") else { return }
-            countTrack = last
+            let last = Int(count.last!)
+            countTrack = last!
         }
     }
     
