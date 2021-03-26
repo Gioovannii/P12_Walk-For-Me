8228ebe232484c643fade94ff835286950bdacdf
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-03-26 11:39:02 +0800

    remove print for trackMapped

--- Walk For Me/Sources/Pace + Map/TrackHistoryTableViewController.swift
+++ Walk For Me/Sources/Pace + Map/TrackHistoryTableViewController.swift
@@ -83,7 +83,6 @@ final class TrackHistoryTableViewController: UITableViewController {
             
             self.trackMapped = track
             self.tableView.reloadData()
-            print("***\(self.trackMapped)")
             self.temporaryLocations = [CLLocation]()
             print(self.temporaryLocations)
         }
