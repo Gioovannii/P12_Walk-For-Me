3e6cd7565c990870a47bd0f4df9c9a4c857c25bd
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-03-29 16:14:31 +0800

    Present alert when view load

--- Walk For Me/Sources/Game/GameViewController.swift
+++ Walk For Me/Sources/Game/GameViewController.swift
@@ -49,6 +49,8 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        presentAlert(title: "Information", message: "Vous pouvez échanger vos pas contre \n de l'argent grâce au logo échange", actionTitle: "OK")
     }
     
     func updateUI(total: Int) {
