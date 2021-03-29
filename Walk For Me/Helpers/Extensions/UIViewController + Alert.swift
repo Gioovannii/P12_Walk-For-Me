d097365f50899f78551cb6a53da7acf64a487aac
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-03-29 16:11:11 +0800

    set an alert for textField

--- Walk For Me/Helpers/Extensions/UIViewController + Alert.swift
+++ Walk For Me/Helpers/Extensions/UIViewController + Alert.swift
@@ -17,4 +17,18 @@ extension UIViewController {
         alert.view.backgroundColor = UIColor(red: 0.290, green: 0.412, blue: 0.714, alpha: 1.0)
 
         present(alert, animated: true)
-    }}
+    }
+    
+    func displayExchangeAlert(handlerExchangeName: @escaping (String?) -> Void) {
+        let alert = UIAlertController(title: "Combien de pas veut tu échanger", message: "", preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.placeholder = "nombre de pas"
+        }
+        let action = UIAlertAction(title: "Echanger maintenant", style: .default, handler: { _ in
+            guard let textField = alert.textFields else { return }
+            handlerExchangeName(textField[0].text)
+        })
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+}
