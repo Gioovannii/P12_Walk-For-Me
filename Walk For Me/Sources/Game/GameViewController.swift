4032614144e1c1dbfd16caac7588916138bda638
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-04-23 16:22:23 +0800

    remove code in action not priority

--- Walk For Me/Sources/Game/GameViewController.swift
+++ Walk For Me/Sources/Game/GameViewController.swift
@@ -111,7 +111,20 @@ class GameViewController: UIViewController {
     
     
     @IBAction func slotButton1Tapped(_ sender: UIButton) {
-        print("Tapped")
-        
+        print("Start planting")
+    }
+}
+
+extension GameViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return vegetableChoice.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return vegetableChoice[row]
     }
 }
