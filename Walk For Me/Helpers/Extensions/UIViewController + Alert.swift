fa2bb473d6bba5f169837a93f0730113dccd98bc
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-04-28 21:17:55 +0800

    miss typo

--- Walk For Me/Helpers/Extensions/UIViewController + Alert.swift
+++ Walk For Me/Helpers/Extensions/UIViewController + Alert.swift
@@ -19,8 +19,8 @@ extension UIViewController {
         present(alert, animated: true)
     }
     
-    func displayExchangeAlert(type: String, handlerExchangeName: @escaping (String?) -> Void) {
-        let alert = UIAlertController(title: "Combien \(type) veut tu échanger", message: "", preferredStyle: .alert)
+    func displayExchangeAlert(type: String, placeHolder: String, handlerExchangeName: @escaping (String?) -> Void) {
+        let alert = UIAlertController(title: "Combien de \(type) veut tu échanger", message: "", preferredStyle: .alert)
         alert.addTextField { textField in
             textField.placeholder = "nombre de pas"
         }
