fa4913d2a9cce4fb13aec12d1332255d24d36201
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-05-01 23:14:33 +0800

    set tomate to local core data

--- Walk For Me/Sources/Game/GameViewController.swift
+++ Walk For Me/Sources/Game/GameViewController.swift
@@ -63,7 +63,8 @@ final class GameViewController: UIViewController {
         
         wheatQuantityLabel.text = "\(coreDataManager?.game?.wheatAmount ?? "0")"
         potatoeQuantityLabel.text = "\(coreDataManager?.game?.potatoeAmount ?? "0")"
-        
+        tomatoesQuantityLabel.text = "\(coreDataManager?.game?.tomatoeAmount ?? "0")"
+
     }
     
     
