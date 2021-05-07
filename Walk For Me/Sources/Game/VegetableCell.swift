6c0a816bd37822406b356d2ea83c706abb86fb92
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-05-07 23:09:18 +0800

    set new file for cell

---
+++ Walk For Me/Sources/Game/VegetableCell.swift
@@ -0,0 +1,18 @@
+//
+//  VegetableCell.swift
+//  Walk_For_Me
+//
+//  Created by Giovanni Gaffé on 2021/5/7.
+//
+
+import UIKit
+
+class VegetableCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+   
+    
+}
