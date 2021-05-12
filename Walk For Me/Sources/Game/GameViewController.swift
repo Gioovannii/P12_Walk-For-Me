b91ebbc697854a5678222032d90b66b8a30d3e78
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-05-12 16:08:22 +0800

    remove spaces

--- Walk For Me/Sources/Game/GameViewController.swift
+++ Walk For Me/Sources/Game/GameViewController.swift
@@ -165,9 +165,7 @@ extension GameViewController: UIPickerViewDataSource, UIPickerViewDelegate {
 
 extension GameViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
-    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { return 10 }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",for: indexPath) as? VegetableCell else { return UICollectionViewCell() }
