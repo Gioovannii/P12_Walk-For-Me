cf6ceacaf8e2fd01fe5a37b8950071566d221012
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-05-31 21:52:16 +0800

    miss typing

--- Walk For Me/Sources/Game/GameViewController.swift
+++ Walk For Me/Sources/Game/GameViewController.swift
@@ -120,7 +120,7 @@ final class GameViewController: UIViewController {
     }
     
     @IBAction func moneyExchangeButtonTapped(_ sender: UIButton) {
-        displayAlert(tag: 1, title: "Que souhaites-tu acheter") { information in
+        displayAlert(tag: 1, title: "Que souhaites-tu acheter ?") { information in
             guard let information = information else { return }
             
             var informationFailed = ""
