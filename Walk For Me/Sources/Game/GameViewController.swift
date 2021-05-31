45b93c3905b0bd9c98f2ed67e2ec7eaf42704008
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-05-31 21:50:34 +0800

    set current value to nil

--- Walk For Me/Sources/Game/GameViewController.swift
+++ Walk For Me/Sources/Game/GameViewController.swift
@@ -84,7 +84,10 @@ final class GameViewController: UIViewController {
         pickerFrame.dataSource = self
         pickerFrame.delegate = self
         
-        alertController.addAction(UIAlertAction(title: "Annuler", style: .cancel))
+        alertController.addAction(UIAlertAction(title: "Annuler", style: .cancel) { _ in
+            self.currentValue = ""
+        })
+       
         alertController.addAction(UIAlertAction(title: "OK", style: .default) { _ in
             information(self.currentValue)
         })
