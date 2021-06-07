33f45392a4c4d93d8ea925b7f822132c17001dfa
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-06-07 10:57:53 +0800

    check where the tag is 1 or and return the nimber of component

--- Walk For Me/Sources/Game/GameViewController.swift
+++ Walk For Me/Sources/Game/GameViewController.swift
@@ -233,7 +233,10 @@ final class GameViewController: UIViewController {
 extension GameViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     func numberOfComponents(in pickerView: UIPickerView) -> Int { 1 }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int { return vegetableChoice.count }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 1 { return vegetableChoice.count
+        } else { return vegetableChoiceMoney.count }
+    }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? { return vegetableChoice[row] }
     
