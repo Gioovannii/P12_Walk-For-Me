396b249302fd9142117991791d6981a74f203ffe
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-06-24 20:12:09 +0800

    set test file

---
+++ Walk For MeTests/ExtensionsTestCase.swift
@@ -0,0 +1,13 @@
+//
+//  Walk_For_MeTests.swift
+//  Walk For MeTests
+//
+//  Created by Giovanni Gaffé on 2021/1/15.
+//
+
+import XCTest
+@testable import Walk_For_Me
+
+class ExtensionsTestCase: XCTestCase {
+
+}
