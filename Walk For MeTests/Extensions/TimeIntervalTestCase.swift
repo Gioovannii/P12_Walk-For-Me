de3dc7b97ce83ee5b7281c9868f159ca6834dcd5
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-06-25 16:36:31 +0800

    set testForTimeInterval

---
+++ Walk For MeTests/Extensions/TimeIntervalTestCase.swift
@@ -0,0 +1,31 @@
+//
+//  TimeIntervalTestCase.swift
+//  Walk_For_MeTests
+//
+//  Created by Giovanni Gaffé on 2021/6/25.
+//
+
+@testable import Walk_For_Me
+import XCTest
+
+class TimeIntervalTestCase: XCTestCase {
+    
+    var coreDataStack: MockCoreDataStack!
+    var coreDataManager: CoreDataManager!
+
+    // MARK: - Test life cycle
+
+    override func setUp() {
+        super.setUp()
+        coreDataStack = MockCoreDataStack()
+        coreDataManager = CoreDataManager(coreDataStack: coreDataStack)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        coreDataManager = nil
+        coreDataStack = nil
+    }
+    
+    
+}
