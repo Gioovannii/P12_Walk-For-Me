085f78fe4d4fec852d8c8db1649fc68932516f26
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-06-25 16:35:23 +0800

    set a mock of coreDataStack

---
+++ Walk For MeTests/CoreDataTests/MockCoreDataStack.swift
@@ -0,0 +1,32 @@
+//
+//  CoreDataManagerTestcase.swift
+//  Walk_For_MeTests
+//
+//  Created by Giovanni Gaffé on 2021/6/24.
+//
+
+import Walk_For_Me
+import CoreData
+
+final class MockCoreDataStack: CoreDataStack {
+    
+    // MARK: - Initializer
+    
+    convenience init() {
+        self.init(modelName: "Reciplease")
+    }
+    
+    override init(modelName: String) {
+        super.init(modelName: modelName)
+        let persistentStoreDescription = NSPersistentStoreDescription()
+        persistentStoreDescription.type = NSInMemoryStoreType
+        let container = NSPersistentContainer(name: modelName)
+        container.persistentStoreDescriptions = [persistentStoreDescription]
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        self.persistentContainer = container
+    }
+}
