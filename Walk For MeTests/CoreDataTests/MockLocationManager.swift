dbcaa0a79b8e8d668a91612f3b17f5f447840b89
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-06-26 09:12:37 +0800

    set mock Location

---
+++ Walk For MeTests/CoreDataTests/MockLocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  MockCLLocationManager.swift
+//  Walk_For_MeTests
+//
+//  Created by Giovanni Gaffé on 2021/6/25.
+//
+@testable import Walk_For_Me
+import CoreLocation
+
+class MockLocationManager  {
+    
+    
+}
