3eaf0097bd03328f4b8c8eaa23c18a03f0aeeb29
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-06-26 09:15:48 +0800

    set Core data manager

---
+++ Walk For MeTests/CoreDataTests/CoreDataManagerTestCase.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataManagerTestCase.swift
+//  Walk_For_MeTests
+//
+//  Created by Giovanni Gaffé on 2021/6/25.
+//
+
+@testable import Walk_For_Me
+import XCTest
+import CoreLocation
+
+class CoreDataManagerTestCase: XCTestCase {
+
+ 
+}
