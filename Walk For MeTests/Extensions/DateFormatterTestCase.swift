7e21404344d0ef3e8b933fc9fd51d45e90652b39
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-07-02 22:25:47 +0800

    Test of data formatter

--- Walk For MeTests/Extensions/DateFormatterTestCase.swift
+++ Walk For MeTests/Extensions/DateFormatterTestCase.swift
@@ -5,8 +5,17 @@
 //  Created by Giovanni Gaffé on 2021/6/25.
 //
 
+@testable import Walk_For_Me
 import XCTest
+import CoreLocation
 
 class DateFormatterTestCase: XCTestCase {
 
+    let dateString = "2021-06-26, 17:54:45 +08:00 "
+    func testGetDateFromString() {
+        let date = DateFormatter.getDateFromString(date: dateString)
+
+        XCTAssert(date == DateFormatter.getDateFromString(date: dateString))
+        
+    }
 }
