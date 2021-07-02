2a2bf262d89681c4fcd3629635c0308fba00a518
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-07-02 22:26:03 +0800

    add some test for clean double

--- Walk For MeTests/Extensions/DoubleExtension.swift
+++ Walk For MeTests/Extensions/DoubleExtension.swift
@@ -5,8 +5,15 @@
 //  Created by Giovanni Gaffé on 2021/6/25.
 //
 
+@testable import Walk_For_Me
 import XCTest
 
 class DoubleExtension: XCTestCase {
 
+    let double = 20.0
+    func test() {
+        let cleanDouble = double.clean
+        
+        XCTAssert(cleanDouble == "20")
+    }
 }
